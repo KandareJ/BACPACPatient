b0c610e066300b52bfdede92c733e53fd59eecf3
KandareJ <jace.kandare@yahoo.com>
2020-06-01 10:53:42 -0600

    Add new component to navigate to
    See: #4

--- source/App.js
+++ source/App.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react'
 import DeviceConnect from './components/DeviceConnect/DeviceConnect'
+import { NavigationContainer } from '@react-navigation/native';
+import ConnectedDevice from './components/ConnectedDevice/ConnectedDevice';
 
 export default class App extends Component {
   render() {
     return(
-      <DeviceConnect />
+      <NavigationContainer>
+	<ConnectedDevice />
+      </NavigationContainer>
     );
   }
 };
