7a7feb55e6c4eff81cd9364e04860c2657d4f8c4
KandareJ <jace.kandare@yahoo.com>
2020-06-04 14:14:03 -0600

    Add relative time to ConnectedDevice

--- source/components/ConnectedDevice/ConnectedDevice.js
+++ source/components/ConnectedDevice/ConnectedDevice.js
@@ -3,15 +3,17 @@ import { View, Text, Image } from 'react-native';
 import { TapGestureHandler } from 'react-native-gesture-handler';
 import Animated from 'react-native-reanimated';
 import { styles } from './styles';
+import { connect } from 'react-redux';
+import { toRelativeTime } from './time';
 
-export default class ConnectedDevice extends Component {
+class ConnectedDevice extends Component {
   constructor(props) {
     super(props);
     this.state = {
       battery: 82,
       storage: 43,
-      lastSync: "12 hours ago",
-      lastPush: "12 hours ago"
+      lastSync: Date.now(),
+      lastPush: Date.now()
     }
   }
 
@@ -24,11 +26,11 @@ export default class ConnectedDevice extends Component {
       <View style={styles.bgView}>
         <Image style={styles.image} source={require('../../../assets/img/BackHarnessWoman.png')} />
         <View style={styles.textView}>
-          <Text style={styles.title}>BacPac Device</Text>
+          <Text style={styles.title}>{this.props.device.name}</Text>
           <Text style={styles.text}>Battery:<Text style={styles.textNotBold}> {this.state.battery}%</Text></Text>
           <Text style={styles.text}>Storage:<Text style={styles.textNotBold}> {this.state.storage}%</Text></Text>
-          <Text style={styles.text}>Last Sync:<Text style={styles.textNotBold}> {this.state.lastSync}</Text></Text>
-          <Text style={styles.text}>Last Push:<Text style={styles.textNotBold}> {this.state.lastPush}</Text></Text>
+          <Text style={styles.text}>Last Sync:<Text style={styles.textNotBold}> {toRelativeTime(this.state.lastSync)}</Text></Text>
+          <Text style={styles.text}>Last Push:<Text style={styles.textNotBold}> {toRelativeTime(this.state.lastPush)}</Text></Text>
         </View>
 
         <TapGestureHandler onHandlerStateChange={this.onButtonStateChange}>
@@ -41,3 +43,11 @@ export default class ConnectedDevice extends Component {
 	  );
   }
 }
+
+const mapStateToProps = (state) => {
+  return {
+    device: state.addedDevice
+  };
+}
+
+export default connect(mapStateToProps)(ConnectedDevice);
