34d862b4894019442de727b59eef72bfa9ae409c
KandareJ <jace.kandare@yahoo.com>
2020-06-22 17:20:22 -0600

    Create BLEProxy

---
+++ source/logic/BLEProxy.js
@@ -0,0 +1,32 @@
+import { BleManager } from 'react-native-ble-plx';
+
+export default class BLEProxy {
+  // public
+  constructor() {
+    this.manager = new BleManager();
+  }
+
+  loggy() {
+    console.log("Hello from BLEProxy!");
+  }
+
+  async scan(callback) {
+    const isOn = await this.manager.state();
+    if (isOn !== 'PoweredOn') {
+      const subscription = this.manager.onStateChange((state) => {
+          if (state === 'PoweredOn') {
+            subscription.remove();
+              this.scan(callback);
+          }
+      }, true);
+    }
+    else {
+      this.manager.startDeviceScan(null, null, callback);
+    }
+  }
+
+  connect() {
+    this.manager.stopDeviceScan();
+  }
+
+}
