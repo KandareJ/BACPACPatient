281f7e32052801432bd7aa65f93a97291f1cc48b
KandareJ <jace.kandare@yahoo.com>
2020-07-07 08:28:43 -0600

    Fix TopBar style to remove red from bottom of screen

--- source/components/TopBar/styles.js
+++ source/components/TopBar/styles.js
@@ -11,7 +11,7 @@ export const styles = StyleSheet.create({
     backgroundColor: 'rgb(70,100,140)',
   },
   bottomSection: {
-    backgroundColor: 'red',// 'white',
+    backgroundColor:  'white',
   },
   bar: {
     width,
