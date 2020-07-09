feb004c7da72eb4db31596609bbafc84fd2e1774
KandareJ <jace.kandare@yahoo.com>
2020-07-09 13:05:01 -0600

    Refactored TopBar
    See #34

--- source/components/TopBar/index.js
+++ source/components/TopBar/index.js
@@ -3,56 +3,47 @@ import { View, Text, Image, TouchableOpacity, SafeAreaView } from 'react-native'
 import { styles } from './styles';
 
 export default class TopBar extends Component {
+  constructor(props) {
+    super(props);
+    this.leftSide = this.leftSide.bind(this);
+  }
+
+  leftSide() {
+    if (this.props.onMenuPress) {
+      return (
+        <TouchableOpacity style={styles.imageContainer} onPress={this.props.onMenuPress}>
+          <Image style={styles.image} source={require('../../../assets/img/menuButton.png')} />
+        </TouchableOpacity>
+      );
+    }
+    else if (this.props.back) {
+      <TouchableOpacity style={styles.imageContainer} onPress={this.props.back}>
+        <Image style={styles.image} source={require('../../../assets/icons/arrow_back_ios.png')} />
+      </TouchableOpacity>
+    }
+  }
+
   render() {
-    if(this.props.onMenuPress) {
-      return (
-        <View style={{flex: 1}}>
-          <SafeAreaView style={styles.topSection}/>
-
-          <SafeAreaView style={styles.screenView}>
-            <View style={styles.bar}>
-              <View style={styles.sideSpace}>
-                <TouchableOpacity style={styles.imageContainer} onPress={this.props.onMenuPress}>
-                  <Image style={styles.image} source={require('../../../assets/img/menuButton.png')} />
-                </TouchableOpacity>
-              </View>
-              <View style={styles.centerSpace}>
-                <Text style={styles.title}>{this.props.title}</Text>
-              </View>
-              <View style={styles.sideSpace} />
+    return (
+      <View style={{flex: 1}}>
+        <SafeAreaView style={styles.topSection}/>
+        <SafeAreaView style={styles.screenView}>
+          <View style={styles.bar}>
+            <View style={styles.sideSpace}>
+              {this.leftSide()}
             </View>
-
-            <View style={styles.children}>
-              {this.props.children}
+            <View style={styles.centerSpace}>
+              <Text style={styles.title}>{this.props.title}</Text>
             </View>
-          </SafeAreaView>
+            <View style={styles.sideSpace} />
+          </View>
 
-          <SafeAreaView style={styles.bottomSection} />
-        </View>
-      );
-    }
-    else {
-      return (
-        <View style={{flex: 1}}>
-          <SafeAreaView style={styles.topSection}/>
-
-          <SafeAreaView style={styles.screenView}>
-            <View style={styles.bar}>
-              <View style={styles.sideSpace} />
-              <View style={styles.centerSpace}>
-                <Text style={styles.title}>{this.props.title}</Text>
-              </View>
-              <View style={styles.sideSpace} />
-            </View>
-
-            <View style={styles.children}>
-              {this.props.children}
-            </View>
-          </SafeAreaView>
-
-          <SafeAreaView style={styles.bottomSection} />
-        </View>
-      );
-    }
+          <View style={styles.children}>
+            {this.props.children}
+          </View>
+        </SafeAreaView>
+        <SafeAreaView style={styles.bottomSection} />
+      </View>
+    );
   }
 }
